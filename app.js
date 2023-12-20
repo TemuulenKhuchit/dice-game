ae7833a1e87996a64bffd355c285444367ef6a18
Temuulen <temuulenkhuchit01@gmail.com>
2023-12-20 09:33:10 +0800

    Make the dice rollable

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шоо яаж буусныг хадгалах хувьсагч хэрэгтэй. 1-6 гэсэн утгыг санамсаргүйгээр үүсгэж хувьсагчид онооно.
-var dice = Math.floor(Math.random() * 6) + 1;
 
 // Програм эхлэхэд бэлтгэе.
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-console.log("Шоо: " + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
